6a762bcc93f95831745a5fabc5e03a1b0abe2116
Claidestho <loris.gulisano@gmail.com>
2022-01-24 09:54:40 +0100

    Mise à jour item.php - Fin de l'exo 2.1

--- item.php
+++ item.php
@@ -4,6 +4,8 @@ $name = "Plateau";
 $price = 10;
 $picture = "https://www.pebbly.fr/361-large_default/plateau-de-service-en-bambou-m-33-x-21-cm.jpg";
 
-echo $price,"<br />", $name, "<br />", $picture;
+echo $price,"<br />", $name, "<br />", "<img src=$picture>";
+
+echo "<br /> Le prix est de $price euros";
 
 ?>
