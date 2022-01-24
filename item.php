7145918f3fff67c5c5343174feee961ceafc9f03
Claidestho <loris.gulisano@gmail.com>
2022-01-24 09:41:07 +0100

    Première version de item.php

---
+++ item.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Plateau";
+$price = 10;
+$picture = "https://www.pebbly.fr/361-large_default/plateau-de-service-en-bambou-m-33-x-21-cm.jpg";
+
+echo $price,"<br />", $name, "<br />", $picture;
+
+?>
