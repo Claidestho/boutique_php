2787929c013182706dd2cbc8e1f3610bd99fa303
Claidestho <loris.gulisano@gmail.com>
2022-01-26 14:23:43 +0100

    Fin de l'exo 5.2

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -5,6 +5,7 @@ include 'my-functions.php';
 $iphone = [
     "name" => "iphone",
     "price" => 45000,
+    "price without VAT" => 0,
     "weight" => 450,
     "discount" => 10,
     "image" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-select-2019-family_GEO_EMEA?wid=882&hei=1058&fmt=jpeg&qlt=80&.v=1567022219953"
@@ -13,6 +14,7 @@ $iphone = [
 $ipad = [
     "name" => "ipad",
     "price" => 15000,
+    "price without VAT" => 0,
     "weight" => 1200,
     "discount" => 5,
     "image" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-202109-gallery-1?wid=2822&hei=2400&fmt=jpeg&qlt=80&.v=1629916218000"
@@ -21,6 +23,7 @@ $ipad = [
 $imac = [
     "name" => "imac",
     "price" => 89500,
+    "price without VAT" => 0,
     "weight" => 3500,
     "discount" => 8,
     "image" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/imac-og-202008?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1594849639000"
@@ -30,6 +33,8 @@ $products = [$iphone, $ipad, $imac];
 
 for ($i = 0; $i < 3; $i++) {
     $products[$i]["price"] = formatPrice($products[$i]["price"]);
+    $products[$i]["price without VAT"] = priceExcludingVAT($products[$i]["price"]);
+
 
 
     echo "<pre>";
