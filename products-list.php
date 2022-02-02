39957e803ea480efebda247508b0b884119b9391
Claidestho <loris.gulisano@gmail.com>
2022-02-02 14:12:22 +0100

    Adjustments to products list

--- products-list.php
+++ products-list.php
@@ -3,7 +3,7 @@
 $products = [
     "ananas" => [
         "name" => "Ananas",
-        "price" => 45000,
+        "price" => 20000,
         "discount_rate" => 50,
         "weight" => 450,
         "discount" => 10,
@@ -12,15 +12,15 @@ $products = [
     "pasteque" => [
         "name" => "Pastèque",
         "price" => 10000,
-        "discount_rate" => 15,
+        "discount_rate" => 50,
         "weight" => 1500,
         "discount" => 5,
         "image" => "https://static.lecomptoirlocal.fr/img/produits/43ad4dfa-6353-451c-aa01-59b6f89b6ddc/large.jpg"
     ],
     "goyave" => [
         "name" => "Goyave",
-        "price" => 89500,
-        "discount_rate" => 65,
+        "price" => 30000,
+        "discount_rate" => 50,
         "weight" => 3500,
         "discount" => 8,
         "image" => "https://delice-exotique.fr/wp-content/uploads/2021/04/IMG-0166.jpg"
